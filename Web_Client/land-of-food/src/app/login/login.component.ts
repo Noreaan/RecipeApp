0966bbfd8923351c7cc373e48b9d21644170cfdc
Michiel <michiel.vanderdonck@gmail.com>
2021-05-01 16:27:55 +0200

    Login, register, reset password screen + main page

--- Web_Client/land-of-food/src/app/login/login.component.ts
+++ Web_Client/land-of-food/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.initFormGroup();
   }
 
+  initFormGroup() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
 }
